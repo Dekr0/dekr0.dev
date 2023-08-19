606638ae2747219277238495fe94b642c42426f6
Dekr0 <dekr0.dk@protonmail.com>
2023-08-19 02:16:02 -0600

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        screens: {
+            'sm': '550px',
+        },
+        fontFamily: {
+
+        },
+        extend: {
+            colors: {
+                'bg': '#1a1b26',
+                'fg': '#a9b1d6',
+                'purple': '#9d7cd8',
+                'cyan': '#7dcfff',
+                'red': '#f7768e',
+                'icon-color': '#7dcfff',
+                'icon-hover': '#9d7cd8',
+            },
+            flexBasis: {
+                'nav-sm': '13.7%',
+            },
+        },
+    },
+    plugins: [],
+}
