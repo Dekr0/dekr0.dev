8105a73700afb67c78cc98d714e8149926f18c39
Dekr0 <dekr0.dk@protonmail.com>
2023-08-27 01:23:18 -0600

    Responsive Nav

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,24 @@
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
+        screens: {
+            'xs': '440px',
+
+            // => @media (min-width: 640px) { ... }
+            'sm': '485px',
+
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
+
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }
+
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
+        },
         fontFamily: {
 
         },
