ee1741b926883712a63ae2dbe6c4c38364b6889f
Dekr0 <dekr0.dk@protonmail.com>
2024-02-29 14:34:20 -0700

    a dedicate table for oauth account

--- src/sqlite.ts
+++ src/sqlite.ts
@@ -4,9 +4,16 @@ export const db = sqlite(":memory:");
 
 db.exec(`CREATE TABLE IF NOT EXISTS user (
     id TEXT NOT NULL,
-    provider_id TEXT NOT NULL UNIQUE,
     username TEXT NOT NULL,
     PRIMARY KEY("id")
+)`);
+
+db.exec(`CREATE TABLE IF NOT EXISTS oauth_account (
+    provider_id TEXT NOT NULL,
+    provider_user_id TEXT NOT NULL,
+    user_id TEXT NOT NULL,
+    PRIMARY KEY (provider_id, provider_user_id),
+    FOREIGN KEY (user_id) REFERENCES user(id)
 )`);
 
 db.exec(`CREATE TABLE IF NOT EXISTS session (
