c6d295058f8e324f9354f4eab0f490dcc1eac498
Dekr0 <dekr0.dk@protonmail.com>
2024-02-29 08:26:01 -0700

    config relative things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: "server",
-  adapter: vercel({
-      imageService: true
-  })
+    integrations: [tailwind()],
+    output: "server",
+    adapter: vercel({
+        imageService: true,
+    }),
+    vite: {
+        optimizeDeps: {
+            exclude: ["oslo"]
+        }
+    }
 });
