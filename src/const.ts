734a26f5b408bab480dd987ba4dca764d5ec595d
Dekr0 <dekr0.dk@protonmail.com>
2024-03-20 15:55:18 -0600

    update env

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,4 @@
 export const prod = import.meta.env.PROD;
 const https = prod ? "https" : "http";
 const addr = prod ? import.meta.env.BACKEND_ADDR : "localhost";
-const port = prod ? import.meta.env.PUBLIC_BACKEND_PORT : 8000;
-export const dataBackend = `${https}://${addr}:${port}`;
+export const dataBackend = `${https}://${addr}`;
