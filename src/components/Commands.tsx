bb0f8669e46f2297c7c058a374f85501aa38a4ef
Dekr0 <dekr0.dk@protonmail.com>
2024-07-09 00:52:02 -0600

    A list of commands components

--- src/components/Commands.tsx
+++ src/components/Commands.tsx
@@ -1,20 +1,103 @@
+import type { JSXElement } from "solid-js";
+import Man from "./Man";
 import quotes from "../quote.mts";
+import { factory } from "../icon.mts";
 
-export function Echo(buffer: string) {
-    return <p class="text-solar-base-1 lg:text-lg font-mono">{buffer}</p>
+
+export type Result = {
+    c: JSXElement
+    e: string
 }
 
-export function NotFound(cmd: string) {
-    return <p class="text-solar-base-1 text-sm sm:text-base lg:text-lg font-mono">
+export function About(): Result {
+    return Man("about");
+}
+
+export function Cd(args?: string): Result {
+    return UnderConstruction("cd");
+}
+
+export function Echo(buffer: string): Result {
+    return {
+        c: <p class="text-solar-base-1 lg:text-lg font-mono">{buffer}</p>,
+        e: ""
+    }
+}
+
+export function CoreDumpCtl(): Result {
+    return {
+        c: (
+        <section id="bugs" class="flex flex-col text-solar-base-1">
+           <p>Avoid using "Control" and "Tab" at the same time when trigger command suggestions. If input is not responding, press "Control", "Alt", or other similar keys to resolve it</p> 
+        </section>
+        ),
+        e: ""
+    };
+}
+
+export function Free(args?: string): Result {
+    return UnderConstruction("free");
+}
+
+export function Ls(args?: string): Result {
+    return UnderConstruction("ls");
+}
+
+export function NeoFetch(args?: string) {
+    return UnderConstruction("neofetch");
+}
+
+export function NotFound(cmd: string): Result {
+    return {
+        c: <p class="text-solar-base-1 text-sm sm:text-base lg:text-lg font-mono">
                 command not found: 
                 <span class="text-solar-yellow-500 font-mono">{cmd}</span>
-            </p>
+            </p>,
+        e: "NOTFOUND"
+    };
 }
 
-export function Quote() {
-    return <p class="italic text-solar-base-1 text-sm sm:text-base lg:text-lg">"{quotes()}"</p>
+export function Quote(): Result {
+    return {
+        c: <p class="italic text-solar-base-1 text-sm sm:text-base lg:text-lg">
+               "{quotes()}"
+           </p>,
+        e: ""
+    };
 }
 
 export function Welcome() {
-    return <p class="text-solar-base-1 lg:text-lg font-mono">type 'help' for a list of commands</p>
+    return (
+        <section id="welcome">
+            <p class="text-solar-base-1 lg:text-lg font-mono">
+                type 'help' for a list of commands.
+            </p>
+            <p class="text-solar-base-1 lg:text-lg font-mono">
+                type 'coredumpctl' for a list of keyboard conflicts caused by Browser default shortcut.
+            </p>
+        </section>
+    )
 }
+
+function UnderConstruction(cmd: string): Result {
+    return {
+        c: <p class="text-solar-base-1 lg:text-lg font-mono">
+               <span class="text-solar-yellow-500">'{cmd}'</span> is still under construction <span class="text-solar-yellow-500 text-3xl">{factory}</span>.
+           </p>,
+        e: ""
+    };
+}
+
+const commands: string[] = [
+    "about"      ,
+    "cd"         ,
+    "coredumpctl",
+    "echo"       ,
+    "free"       ,
+    "ls"         ,
+    "neofetch"   ,
+    "man"        ,
+    "quote"   
+];
+
+export default commands;
