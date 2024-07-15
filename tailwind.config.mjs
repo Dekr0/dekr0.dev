c22ce42039d3b2ee71d3b1be84d62c85f126e276
Dekr0 <dekr0.dk@protonmail.com>
2024-07-15 04:15:39 -0600

    smaller screen viewport

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,12 @@
-import plugin from 'tailwindcss';
 import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+        screens: {
+            "xs": "400px",
+        },
         extend: {
             colors: {
                 solar: {
