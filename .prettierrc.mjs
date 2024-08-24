0cbd5d83239f247ad56af90db523309eb3ac6626
Dekr0 <dekr0.dk@protonmail.com>
2024-08-24 02:01:18 -0600

    prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,12 @@
+export default {
+    tabWidth: 4,
+    plugins: ["prettier-plugin-astro"],
+    overrides: [
+        {
+            files: "*.astro",
+            options: {
+                parser: "astro",
+            },
+        },
+    ],
+};
