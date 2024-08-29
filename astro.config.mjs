9f580e773cfaf30e509a08de46962e61eaf50d0a
Dekr0 <dekr0.dk@protonmail.com>
2024-08-28 23:38:42 -0600

    sql adapter, xterm

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,15 @@ import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-import { shield } from '@kindspells/astro-shield'
-
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs(), shield({})],
-  security: {
-    checkOrigin: true
-  },
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+    integrations: [tailwind(), solidJs()],
+    security: {
+      checkOrigin: true
+    },
+    output: "server",
+    adapter: node({
+      mode: "standalone"
+    })
 });
