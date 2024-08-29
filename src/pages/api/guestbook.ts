33dd1bc2989b4300dc4387bb6405f13fa6898cdb
Dekr0 <dekr0.dk@protonmail.com>
2024-08-28 23:42:56 -0600

    REST guestbook

--- src/pages/api/guestbook.ts
+++ src/pages/api/guestbook.ts
@@ -1,19 +1,40 @@
-import type { APIRoute } from "astro";
+import type { APIContext, APIRoute } from "astro";
+import { lucia } from "src/auth";
+import getLogger from "src/logger";
+import db from "src/db";
 
-export const GET: APIRoute = async ({ params, request }) => {
-    const encoder = new TextEncoder();
-    const eventStream = new ReadableStream({
-        async start(controller) {
-            controller.enqueue(encoder.encode(`data: an event\n\n`))
-            await new Promise((r) => setTimeout(r, 10000))
-            controller.close()  
+const logger = getLogger().child({ filename: "/pages/api/guestbook" });
+
+export const POST: APIRoute = async (context: APIContext) => {
+    const sessionId = context.cookies.get(lucia.sessionCookieName)?.value;
+    if (!sessionId) {
+        logger.info("Incoming request has no session cookie");
+        return new Response(null, { status: 400 });
+    }
+
+    const { session, user } = await lucia.validateSession(sessionId);
+    if (session) {
+        const formData = await context.request.formData();
+        const content = formData.get("comment");
+        if (!content || typeof content !== "string" || content.length < 2) {
+            return new Response(null, { status: 400 });
         }
-    });
-    return new Response(eventStream, {
-        headers: {
-            Connection: 'keep-alive',
-           'Content-Encoding': 'none',
-           'Cache-Control': 'no-cache, no-transform',
-           'Content-Type': 'text/event-stream; charset=utf-8',
-    }});
+
+        try {
+            const comment = await db.postComment(user.username, content);
+
+            return new Response(JSON.stringify(comment), { status: 200 });
+        } catch (err) {
+            if (err instanceof Error) logger.error(err);
+
+            return new Response("Interal Server Error", { status: 500 });
+        }
+    }
+
+    logger.info("Incoming request with invalid session id");
+    return new Response(null, { status: 401 });
+};
+
+export const GET: APIRoute = async () => {
+    return new Response(JSON.stringify(await db.getAllComments()));
 }
