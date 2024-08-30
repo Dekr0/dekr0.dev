575ad3a3bbceec8aa38008536a31437050943d73
Dekr0 <dekr0.dk@protonmail.com>
2024-08-30 00:27:01 -0600

    unsafe way to check frontend is auth or not; twitter OAuth

---
+++ src/pages/api/auth/oauth/callback/twitter.ts
@@ -0,0 +1,60 @@
+import { OAuth2RequestError } from "arctic";
+import type { APIContext } from "astro";
+import { twitter } from "src/auth";
+import getLogger from "src/logger";
+
+const logger = getLogger().child({
+    filename: "/api/auth/oauth/callback/github.ts",
+    function: "GET",
+});
+
+type TwitterUser = {
+    id: string,
+    name: string,
+    username: string
+};
+
+const api = "https://api.twitter.com/2/users?user.fields=id,name,username";
+
+export async function GET(context: APIContext) {
+    const code = context.url.searchParams.get("code");
+    const state = context.url.searchParams.get("state");
+    const storedState = context.cookies.get("twitter_oauth_state")?.value ?? 
+        null;
+    const codeVerifier = context.cookies.get("code_verifier")?.value ?? null;
+
+    if (!state || !storedState || !code || storedState !== state || !codeVerifier) {
+		return new Response(JSON.stringify({
+            reason: "Encounter server side error during Twitter OAuth"
+        }), {
+			status: 400
+		});
+	}
+
+    try {
+        const tokens = await twitter.validateAuthorizationCode(code, 
+                                                               codeVerifier);
+        const headers = { Authorization: `Bearer ${tokens.accessToken}` };
+        const response = await fetch(api, { headers: headers });
+        const user: TwitterUser = await response.json();
+
+        logger.info(user);
+
+        return context.redirect("/guestbook");
+    } catch(e) {
+        if (e instanceof OAuth2RequestError) {
+            logger.info(e.message);
+            logger.info(e.description);
+            const reason = JSON.stringify({
+                reason: "Encounter server side error during GitHub OAuth",
+            });
+            return new Response(reason, { status: 400 });
+        }
+
+        logger.info(e);
+        const reason = JSON.stringify({
+            reason: "Unkonw server side error during GitHub OAuth",
+        });
+        return new Response(reason, { status: 500 });
+    }
+}
