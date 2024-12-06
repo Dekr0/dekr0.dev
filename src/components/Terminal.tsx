d88e8926c474630bc40df02c7430df83d280e815
Dekr0 <dekr0.dk@protonmail.com>
2024-12-06 06:02:20 -0700

    font accessibility adjustment

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -340,7 +340,7 @@ export default function Terminal() {
         <main
             onMouseUp={onMouseUp}
             onTouchEnd={onTouchEnd}
-            class="flex flex-col gap-2 w-5/6 sm:w-9/12 md:w-8/12 lg:w-7/12 xl:w-6/12 2xl:w-5/12 mx-auto mb-4 py-4 h-screen text-solar-base-1 text-sm xs:text-base font-mono selection:bg-solar-base-1 selection:text-solar-base-04"
+            class="flex flex-col gap-2 w-5/6 sm:w-9/12 md:w-8/12 lg:w-7/12 xl:w-6/12 2xl:w-5/12 mx-auto mb-4 py-4 h-screen text-solar-base-1 text-xs md:text-sm lg:text-base font-mono selection:bg-solar-base-1 selection:text-solar-base-04"
         >
             <section
                 id="history-prompt"
