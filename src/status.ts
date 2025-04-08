75dc3a0b4b2315bbd67fa48d6970bccedeb73f39
Dekr0 <dekr0.dk@protonmail.com>
2025-04-08 02:52:03 -0600

    change .mts to ts; add reading status

---
+++ src/status.ts
@@ -0,0 +1,53 @@
+export type Status = {
+    name: string
+    url?: string
+}
+
+export const learning: Status[] = [
+    {
+        name: "Performance and Optimization (Performance-Aware Programming Series)",
+        url: "https://www.computerenhance.com/p/table-of-contents/",
+    },
+    {
+        name: "Zig",
+        url: "https://ziglang.org/learn/"
+    },
+    {
+        name: "Odin",
+        url: "https://odin-lang.org/docs/overview/"
+    }
+]
+
+export const reading: Status[] = [
+    {
+        name: "Go 100 Mistakes",
+        url: "https://100go.co/"
+    },
+    {
+        name: "Memory Allocation Strategies",
+        url: "https://www.gingerbill.org/series/memory-allocation-strategies/"
+    },
+    {
+        name: "Untangling Lifetimes: The Arena Allocator",
+        url: "https://www.rfleury.com/p/untangling-lifetimes-the-arena-allocator"
+    },
+]
+
+export const working: Status[] = [
+    {
+        name: "Helldivers 2 Audio Modding Tool",
+        url: "https://github.com/RaidingForPants/hd2-audio-modder"
+    },
+    {
+        name: "Wwise Teller",
+        url: "https://github.com/Dekr0/wwise-teller"
+    },
+    {
+        name: "Helldivers 2 Wwise Audio Asset Database",
+        url: "http://github.com/Dekr0/hd2_audio_db"
+    },
+    {
+        name: "Modding Helldivers 2 SFX",
+        url: "https://next.nexusmods.com/profile/Dekw0/mods"
+    }
+]
